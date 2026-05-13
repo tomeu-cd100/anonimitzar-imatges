4d96a08a694f2d1998636641b95a8d3eef5cb903
Riera Forteza <briera2@edu.gencat.cat>
2026-05-13 13:49:06 +0200

    fix: empaquetar tot en un sol index.html per evitar problemes de rutes a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/anonimitzar-imatges/',
+  plugins: [react(), viteSingleFile()],
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
-    assetsDir: 'static',
   },
 })
